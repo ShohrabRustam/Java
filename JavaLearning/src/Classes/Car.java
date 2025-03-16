a52fd9fb18980a755a88f48790cd72b28e2d66be
shorab.rustam <shorab.rustam@mpokket.com>
2025-03-16 21:04:01 +0530

    create basic program in java

---
+++ JavaLearning/src/Classes/Car.java
@@ -0,0 +1,16 @@
+package Classes;
+
+
+public class Car {
+    public String brand;
+    public Integer speed;
+    public String color;
+
+    public void start(){
+        System.out.printf("Car has been start\n");
+        System.out.printf("Car Brand "+ brand + " \n");
+        System.out.printf("Car speed "+ speed + " \n");
+        System.out.printf("Car Color "+ color + " \n");
+
+    }
+}
--- JavaLearning/src/Main.java
+++ JavaLearning/src/Main.java
@@ -1,5 +1,13 @@
+import Classes.Car;
+
 public class Main {
     public static void main(String[] args) {
+
+        Car c1 = new Car();
+        c1.brand = "BMW";
+        c1.color = "Red";
+        c1.speed = 50;
+        c1.start();
         System.out.println("Hello, World!");
     }
 }
