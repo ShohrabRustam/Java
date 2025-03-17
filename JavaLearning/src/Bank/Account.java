97cb26bcaa7787515084ef5b78344885a200b4d3
shorab.rustam <shorab.rustam@mpokket.com>
2025-03-17 20:25:42 +0530

    encaptulation

--- JavaLearning/src/Bank/Account.java
+++ JavaLearning/src/Bank/Account.java
@@ -1,4 +1,13 @@
 package Bank;
 
 public class Account {
+    public double balance;
+}
+
+class Main{
+        public static void main(String[] args){
+            Account account = new Account();
+            account.balance = 50;
+            System.out.println("Amount is :" + account.balance);
+        }
 }
