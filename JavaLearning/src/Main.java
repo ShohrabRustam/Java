3f91ecab127298ef76383b6ccf762eb9012e37e3
shorab.rustam <shorab.rustam@mpokket.com>
2025-03-17 12:24:49 +0530

    refector code

--- JavaLearning/src/Main.java
+++ JavaLearning/src/Main.java
@@ -1,18 +1,34 @@
-import Classes.Inheritance.ElectricBike;
+import Classes.Inheritance.*;
 
 public class Main {
     public static void main(String[] args) {
+        classObj();
+        inheitance();
+        multiInheitance();
+    }
 
-//        Car c1 = new Car();
-//        c1.brand = "BMW";
-//        c1.color = "Red";
-//        c1.speed = 50;
-//        c1.start();
+    private static void classObj(){
+        Car c1 = new Car();
+        c1.brand = "BMW";
+        c1.color = "Red";
+        c1.speed = 50;
+        c1.start();
+    }
+
+
+    private  static void inheitance(){
+        Bike eb1 = new Bike();
+        eb1. speed= 50;
+        eb1.color = "Black";
+        eb1.bikeDetails();
+
+    }
+
+    private  static void multiInheitance(){
         ElectricBike eb1 = new ElectricBike();
         eb1. speed= 50;
         eb1.color = "Black";
         eb1.power = 10;
         eb1.electricBikeDetails();
-//        System.out.println("Hello, World!");
     }
 }
