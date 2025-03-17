796b27a307281950247f691a6ca5107bbd315164
shorab.rustam <shorab.rustam@mpokket.com>
2025-03-17 16:30:38 +0530

    Override annotation

--- JavaLearning/src/Classes/Polymorphism/Dog.java
+++ JavaLearning/src/Classes/Polymorphism/Dog.java
@@ -1,6 +1,8 @@
 package Classes.Polymorphism;
 
 public class Dog  extends Animal {
+
+    @Override
     public void  speak(){
         System.out.println( this.getClass().getSimpleName() + " Speak " +  "Bhoo Bhoo");
     }
