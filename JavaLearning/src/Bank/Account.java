c7665df8b56a4f834febc05c324811a071901dfd
shorab.rustam <shorab.rustam@mpokket.com>
2025-03-17 20:27:52 +0530

    exm of encaptulation

--- JavaLearning/src/Bank/Account.java
+++ JavaLearning/src/Bank/Account.java
@@ -1,13 +1,23 @@
 package Bank;
 
 public class Account {
-    public double balance;
+
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
 }
 
 class Main{
         public static void main(String[] args){
             Account account = new Account();
-            account.balance = 50;
-            System.out.println("Amount is :" + account.balance);
+            account.setBalance(50);
+            System.out.println("Amount is :" + account.getBalance());
         }
 }
