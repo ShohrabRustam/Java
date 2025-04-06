f354ce9752d6fef2696df28099edf7b925b725b7
shorab.rustam <shorab.rustam@mpokket.com>
2025-04-06 12:08:10 +0530

    linkedHashMap and linedHashSet

--- JavaLearning/src/Main.java
+++ JavaLearning/src/Main.java
@@ -5,6 +5,8 @@ import Classes.Polymorphism.Overriding.Cat;
 import Classes.Polymorphism.Overriding.Dog;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 
 public class Main {
@@ -23,6 +25,25 @@ public class Main {
     }
 
     private static void sequenceCollection(){
+        arrayList();
+        linedHashSet();
+        linkedHashMap();
+
+    }
+
+    private static void arrayList(){
+        LinkedHashSet set = new LinkedHashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        System.out.println("set: " + set);
+        System.out.println("first set : "+ set.getFirst());
+        System.out.println("last set : "+set.getLast());
+        System.out.println("reverse set : "+set.reversed());
+    }
+
+    private static void linedHashSet(){
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -37,6 +58,21 @@ public class Main {
         System.out.println("after remove first list : "+list.removeFirst());
         System.out.println("after remove last " + list.removeLast());
         System.out.println("list: "+ list);
+    }
+
+    private static void linkedHashMap(){
+        LinkedHashMap map = new LinkedHashMap<>();
+        map.put(1,2);
+        map.put(1,2);
+        map.put(3,2);
+        map.put("Data",2);
+
+        System.out.println("set: " + map.firstEntry());
+        System.out.println("first set : "+ map.lastEntry());
+        System.out.println("map reverse : "+ map.reversed());
+        System.out.println("sequencedKeySet set : "+ map.sequencedKeySet());
+        System.out.println("sequencedValues set : "+ map.sequencedValues());
+        System.out.println("sequencedEntrySet : "+ map.sequencedEntrySet());
 
     }
 
