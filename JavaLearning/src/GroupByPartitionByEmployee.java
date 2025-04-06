4329689819a5a0de17298d81b5ceb217d78a4062
shorab.rustam <shorab.rustam@mpokket.com>
2025-04-06 21:56:05 +0530

    partition by

--- JavaLearning/src/GroupByPartitionByEmployee.java
+++ JavaLearning/src/GroupByPartitionByEmployee.java
@@ -54,17 +54,23 @@ public class GroupByPartitionByEmployee {
 
         employeeArrayList.add(new GroupByPartitionByEmployee("A", "IT", 100000));
         employeeArrayList.add(new GroupByPartitionByEmployee("B", "TECH", 100000));
-        employeeArrayList.add(new GroupByPartitionByEmployee("C", "Fin", 100000));
+        employeeArrayList.add(new GroupByPartitionByEmployee("C", "Fin", 1000));
         employeeArrayList.add(new GroupByPartitionByEmployee("F", "Legal Team", 100000));
         employeeArrayList.add(new GroupByPartitionByEmployee("D", "IT", 100000));
         employeeArrayList.add(new GroupByPartitionByEmployee("G", "Legal Team", 100000));
         employeeArrayList.add(new GroupByPartitionByEmployee("H", "Legal Team", 100000));
-
         Map<String,List<GroupByPartitionByEmployee>> emmployeeByDepartment = employeeArrayList.stream().collect(Collectors.groupingBy(GroupByPartitionByEmployee::getDepartment));
-
         emmployeeByDepartment.forEach((department,employees)->{
             System.out.println("Department : " + department);
             employees.forEach(System.out::println);
         });
+
+        Map<Boolean,List<GroupByPartitionByEmployee>> emmployeePartitionBy = employeeArrayList.stream().collect(Collectors.partitioningBy(employee-> employee.getSalary()>=20000));
+
+        System.out.println("Empoyee salary greater than 20K");
+        emmployeePartitionBy.get(true).forEach(System.out::println);
+
+        System.out.println("Empoyee salary less than 20K\n");
+        emmployeePartitionBy.get(false).forEach(System.out::println);
     }
 }
